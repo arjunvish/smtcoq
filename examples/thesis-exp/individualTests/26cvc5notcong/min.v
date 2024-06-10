ab863de05c7c0273e93634c6f4193b060ca4c047
arjunvish <arjun.viswanathan23@gmail.com>
2024-06-09 20:56:59 -0500

    minor

--- examples/thesis-exp/individualTests/26cvc5notcong/min.v
+++ examples/thesis-exp/individualTests/26cvc5notcong/min.v
@@ -4,6 +4,6 @@ Require Import Bool.
 
 
 Section test9.
-  Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/thesis-exp/individualTests/26cvc5/min.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/thesis-exp/individualTests/26cvc5/min.pf".
+  Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/thesis-exp/individualTests/26cvc5notcong/min.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/thesis-exp/individualTests/26cvc5notcong/min.pf".
 End test9.
 
