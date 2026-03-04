3778d0d85418437a0151dcc225a1e1d22d5e4728
poririn_ubuntu <polina.kozyarchuk@gmail.com>
2026-03-04 15:20:54 -0500

    restructuring

--- examples/debuggerPolina/ex1/ex1run.v
+++ examples/debuggerPolina/ex1/ex1run.v
@@ -7,8 +7,8 @@ Local Open Scope int31_scope.
 Section ex1run.
 
  Parse_certif_verit t_i t_func t_atom t_form root used_roots trace 
- "ex1.smt2" 
- "ex1.pf". 
+ "ex1/ex1.smt2" 
+ "ex1/ex1.pf". 
 
  Definition nclauses := Eval vm_compute in (match trace with Certif a _ _ => a end). (* Size of the state *)
  Print nclauses.
@@ -18,4 +18,27 @@ Section ex1run.
  Print conf.
 
  Eval vm_compute in List.length (fst c). (* No. of steps in certificate *) 
+
+ Eval vm_compute in (Form.check_form t_form && Atom.check_atom t_atom && Atom.wt t_i t_func t_atom).
+
+ Definition s0 := Eval vm_compute in (add_roots (S.make nclauses) root used_roots).
+ Print s0.
+
+ Eval vm_compute in List.nth 0 (fst c) _.
+
+ Definition s1 := Eval vm_compute in (step_checker s0 (List.nth 0 (fst c) (CTrue t_func t_atom t_form 0))). 
+ Print s1. 
+
+
+ Eval vm_compute in List.nth 1 (fst c) _.
+
+ Definition s2 := Eval vm_compute in (step_checker s1 (List.nth 1 (fst c) (CTrue t_func t_atom t_form 0))). 
+ Print s2. 
+
+
+ Eval vm_compute in List.nth 2 (fst c) _.
+
+ Definition s3 := Eval vm_compute in (step_checker s2 (List.nth 2 (fst c) (CTrue t_func t_atom t_form 0))). 
+ Print s3. 
+
 End ex1run.
